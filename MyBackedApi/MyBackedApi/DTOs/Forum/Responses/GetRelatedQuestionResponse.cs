25d720424da3e314c2cd1141622089949023136c
LiviaTomaNeagu <livia.toma@student.unitbv.ro>
2025-02-01 14:38:22 +0200

    [Feat] Added simple search algorithm for related questions

---
+++ MyBackedApi/MyBackedApi/DTOs/Forum/Responses/GetRelatedQuestionResponse.cs
@@ -0,0 +1,12 @@
+﻿using MyBackedApi.Enums;
+
+namespace MyBackedApi.DTOs.Forum.Responses
+{
+    public class GetRelatedQuestionResponse
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public int AnswersCount { get; set; }
+        public TopicEnum Topic { get; set; }
+    }
+}
